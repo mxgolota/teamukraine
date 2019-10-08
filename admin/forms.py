fea0552b1f24ab2d76fda1dc935246c8f308c2aa
Max Golota <mxgolota@gmail.com>
2019-10-08 13:33:28 +0300

    Added new report
    Report clubs_membership_changes added

--- admin/forms.py
+++ admin/forms.py
@@ -1,9 +1,15 @@
-from wtforms import Form, StringField, TextAreaField, validators, SelectField
+from wtforms import Form, SelectField
 from wtforms.fields.html5 import DateTimeLocalField
-from datetime import datetime
 
 
 class PlayersCountDynamicsForm(Form):
     begin_date = DateTimeLocalField('Дата початку', format='%Y-%m-%dT%H:%M')
     end_date = DateTimeLocalField('Дата закінчення', format='%Y-%m-%dT%H:%M')
     team = SelectField('Команда')
+
+
+class ClubsMembershipChangesForm(Form):
+    begin_date = DateTimeLocalField('Дата початку', format='%Y-%m-%dT%H:%M')
+    end_date = DateTimeLocalField('Дата закінчення', format='%Y-%m-%dT%H:%M')
+    team = SelectField('Команда')
+
