d244d76b213d1eaaecbce8c3f95b741c02a78220
Max Golota <mxgolota@gmail.com>
2020-09-14 14:25:41 +0300

    UCC 2020 Rapid
    Added tournament tables for UCC 2020 Rapid : Div A, Div B, Div U1600

--- tournaments/tournaments_app.py
+++ tournaments/tournaments_app.py
@@ -372,3 +372,32 @@ def ucc2020_blitz():
     tournament_table.sort_values(by=['Загалом'], ascending=False, inplace=True)
 
     return render_template("ucc2019.html", best_players=best_players, rounds=rounds, tournament_table=tournament_table)
+
+
+@tournaments_bp.route("/<tournament_id>")
+def ucc_tournament(tournament_id):
+    with engine.connect() as conn:
+        result = conn.execute("call usp_stat_ucc_best_players ({0})".format(tournament_id))
+        best_players_result = [row for row in result]
+        best_players_columns = result.keys()
+
+    with engine.connect() as conn:
+        result = conn.execute("call usp_stat_ucc_rounds ({0})".format(tournament_id))
+        rounds_result = [row for row in result]
+        rounds_result_columns = result.keys()
+
+    best_players = pd.DataFrame(best_players_result, columns=best_players_columns)
+    rounds = pd.DataFrame(rounds_result, columns=rounds_result_columns)
+    first_teams = rounds[['round_id', 'match_id', 'team1_name', 'team1_result', 'team2_name']]
+    first_teams = first_teams.rename(columns={"team1_name": "team_name", "team1_result": "team_result", "team2_name": "opponent_name"})
+    second_teams = rounds[['round_id', 'match_id', 'team2_name', 'team2_result', 'team1_name']]
+    second_teams = second_teams.rename(columns={"team2_name": "team_name", "team2_result": "team_result", "team1_name": "opponent_name"})
+    tournament_table = pd.concat([first_teams, second_teams]).reset_index()
+
+    tournament_table = pd.pivot_table(tournament_table, columns=['opponent_name'], index=['team_name'], values='team_result').reset_index()
+
+    tournament_table['Загалом'] = tournament_table.sum(axis=1)
+    tournament_table.reset_index(inplace=True)
+    tournament_table.sort_values(by=['Загалом'], ascending=False, inplace=True)
+
+    return render_template("ucc2019.html", best_players=best_players, rounds=rounds, tournament_table=tournament_table)
